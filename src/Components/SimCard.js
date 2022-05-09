2b745dc3621042c8ac69bdd7270ff06152deb23c
Umair <mumairboota007@gmail.com>
2022-05-09 15:18:19 +0500

    adding new features

--- src/Components/SimCard.js
+++ src/Components/SimCard.js
@@ -6,21 +6,36 @@ export default function SimCard({ sNumber }) {
     <div>
       <div className="container mb-2 flex mx-auto w-full items-center justify-center">
         <ul className=" p-4">
-          <li >
+          <li>
             <div className="select-none flex flex-1 items-center duration-500 ease-in-out transition-shadow Form hover:-translate-y-2 rounded-2xl border-2 p-6 hover:shadow-2xl ">
               <div className=" pl-1 mr-16 p-0">
                 <div className="font-medium">
-                  <div >
+                  <div>
                     <h2 className="inline-flex">
                       Captain: Ali
                       <img className="ml-32 w-6" src={loc} alt="" />
                       <label htmlFor="">Allama Iqbal Town</label>
                     </h2>
-                    <div className=" ml-3 border-4 rounded-lg">
-                      {sNumber}-0000000
-                      <button className="hover:text-white font-[Poppins] rounded-lg border-2 border-green-600 md:ml-8  hover:bg-green-400 duration-500">
-                        Buy Now
-                      </button>
+                    <br />
+                    <div className="inline-flex">
+                      <div className=" border-4 rounded-lg mr-4 mt-5">
+                        {sNumber}-0000000
+                        <button className="hover:text-white  text-sm rounded-xl border-2 border-green-600 md:ml-8  hover:bg-green-400 duration-500">
+                          Buy Now
+                        </button>
+                      </div>
+                      <div className=" border-4 rounded-lg mr-4 mt-5">
+                        {sNumber}-0000000
+                        <button className="hover:text-white text-sm rounded-xl border-2 border-green-600 md:ml-8  hover:bg-green-400 duration-500">
+                          Buy Now
+                        </button>
+                      </div>
+                      <div className=" border-4 rounded-lg mr-4 mt-5">
+                        {sNumber}-0000000
+                        <button className="hover:text-white text-sm rounded-xl border-2 border-green-600 md:ml-8  hover:bg-green-400 duration-500">
+                          Buy Now
+                        </button>
+                      </div>
                     </div>
 
                     {/* <h3 className="text-sm inline-flex ml-32 text-gray-500 font-serif ">
